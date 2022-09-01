9299973a8c40225a5c19f1f99651253778ca4605
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-01 12:45:50 -0400

    Hello World

---
+++ Module_1/HelloWorldApp/ZuckHelloWorld.java
@@ -0,0 +1,8 @@
+public class ZuckHelloWorld {
+    public static void main(String[] args) {
+        String message = "Hello, World";
+        String first_name = "Cheyenne ";
+        String last_name = "Zuck";
+        System.out.println(message + " from " + first_name + last_name + "!");
+    }
+}
