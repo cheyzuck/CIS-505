b42bb2ac9de1ee57dafa3d84bac676adaba17ea6
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-08 20:13:06 -0400

    TeamApp Start

---
+++ Module_2/TeamApp/Team.java
@@ -0,0 +1,27 @@
+package Module_2.TeamApp;
+
+public class Team {
+    private String teamName;
+    private String players;
+    private int playerCount = 0;
+
+    public Team() {
+
+    }
+
+    public void addPlayer() {
+
+    }
+
+    public String getPlayers() {
+        return players;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+}
