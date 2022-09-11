9dca9830c6cf52f2838356098e98c329e374c6f2
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-10 20:19:43 -0400

    Build 1

--- Module_2/TeamApp/Team.java
+++ Module_2/TeamApp/Team.java
@@ -2,18 +2,21 @@ package Module_2.TeamApp;
 
 public class Team {
     private String teamName;
-    private String players;
+    private String[] players = new String[20];
     private int playerCount = 0;
 
-    public Team() {
-
+    public Team(String teamName, String[] players, int playerCount) {
+        this.teamName = teamName;
+        this.players = players;
+        this.playerCount = playerCount;
     }
 
-    public void addPlayer() {
-
+    public void addPlayer(String player) {
+        players[playerCount] = player;
+        playerCount++;
     }
 
-    public String getPlayers() {
+    public String [] getPlayers() {
         return players;
     }
 
