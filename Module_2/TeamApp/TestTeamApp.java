76e5d3bd56a13c2a07dbc390e29272bb15132c97
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-11 23:44:44 -0400

    Final

--- Module_2/TeamApp/TestTeamApp.java
+++ Module_2/TeamApp/TestTeamApp.java
@@ -5,38 +5,39 @@ import java.util.*;
 public class TestTeamApp{
 
     public static void main(String[] args){
-            System.out.println("Welcome to the Sports Team App.");
-            Scanner userIn = new Scanner(System.in);
-        while (true){
             System.out.println("");
-            System.out.println("Please enter a team name: ");
+            System.out.println("  Welcome to the Sports Team App.");
+        while (true){
+            Scanner userIn = new Scanner(System.in);
+            System.out.println("");
+            System.out.print("  Please enter a team name: ");
             String teamName = userIn.next();
 
-            System.out.println("\nPlease enter the names of the players: ");
-            System.out.println("Hint: Use commas for multiple players. Do not use spaces.");
-            System.out.println("Example: Aaron,Allen,Smithy\n");
+            System.out.println("\n  Please enter the names of the players: ");
+            System.out.print("    Hint: Use commas for multiple players. Do not use spaces. Example: Aaron,Allen,Smithy: ");
             Scanner userIn_02 = new Scanner(System.in);
             String players = userIn_02.next();
 
             Team newTeam = new Team(teamName);
             newTeam.addPlayer(players);
-            String [] player = players.split(",");
+            String[] player = players.split(",");
 
             System.out.println("");
-            System.out.println("---Team Summary---");
-            System.out.println("Number of players on team: " +newTeam.getPlayerCount());
-            System.out.println("Players on team:");
-            String [] playerArray = newTeam.getPlayers();
+            System.out.println("  --Team Summary--");
+            System.out.println("  Number of players on team: " + newTeam.getPlayerCount());
+            System.out.print("  Players on team: ");
+            String[] playersArray = newTeam.getPlayers();
             for (int i=0; i < newTeam.getPlayerCount(); i++)
-                System.out.print(playerArray[i] +", ");
+                System.out.print(playersArray[i] +", ");
 
             System.out.println("");
             Scanner userIn_Continue = new Scanner(System.in);
-            System.out.println("\nWould you like to continue? Please type 'y' or 'n'.");
+            System.out.print("\n  Continue? y/n: ");
             String userDecision = userIn_Continue.next(); 
             if (userDecision.equalsIgnoreCase("n")){;
                 System.out.println("");
-                System.out.println("End of line.");
+                System.out.println("");
+                System.out.println("  End of line.");
                 System.out.println("");
                 break;
             }
