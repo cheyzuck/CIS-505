737190700dcd3fb29dc62564594de667c9ef4416
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-24 13:00:18 -0400

    Initial

--- Module_4/BowlingShopApp/Ball.java
+++ Module_4/BowlingShopApp/Ball.java
@@ -1,5 +1,20 @@
 package Module_4.BowlingShopApp;
 
-public class Ball {
-    
+public class Ball extends Product{
+    private String color;
+
+    public Ball(){
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String newColor){
+        this.color = newColor;
+    }
+
+    public String toString(){
+        return "  Color: "+getColor();
+    }
 }
