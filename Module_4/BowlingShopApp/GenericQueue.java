9ce8be8240dc927f3512494d2e7152d9d9c08a2f
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-25 21:31:31 -0400

    5/7 classes done with citations and comments

--- Module_4/BowlingShopApp/GenericQueue.java
+++ Module_4/BowlingShopApp/GenericQueue.java
@@ -1,18 +1,25 @@
+/* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University. 
+ 
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+
+*/
 package Module_4.BowlingShopApp;
 
 import java.util.*;
 
 public class GenericQueue<E> {
-    private LinkedList<E> list = new LinkedList<E>();
+    private LinkedList<E> list = new LinkedList<E>(); /* This establishes a new LinkedList as a Generic Queue. */
 
+    /* This method adds an item to the queue. */
     public void enqueue(E item){
         list.addFirst(item);
     }
+    /* This method removes an item from the queue. */
     public void dequeue(E item){
         list.removeFirst();
     }
+    /* This method returns the size of the queue. */
     public int size(){
         return list.size();
     }
-    
-}
+}/* End Generic Queue Class. */
