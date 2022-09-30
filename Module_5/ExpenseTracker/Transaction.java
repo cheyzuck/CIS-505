bdf083e12951853bccb377b15cfb7c097d082d5c
Cheyenne <chzuck@my365.bellevue.edu>
2022-09-30 17:41:10 -0400

    Build Continue 1

--- Module_5/ExpenseTracker/Transaction.java
+++ Module_5/ExpenseTracker/Transaction.java
@@ -3,9 +3,37 @@ package Module_5.ExpenseTracker;
 import java.text.*;
 
 public class Transaction {
-    SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
-    private String date = String.format(dateFormat.format(date));
+    private String date;
     private String description;
     private double amount = 0;
+
+    public Transaction(){
+    }
+
+    public Transaction(String date, String description, double amount){
+    }
+
+    public void setDate(String newDate){
+        this.date = newDate;
+    }
+    public void setDescription(String newDescription){
+        this.description = newDescription;
+    }
+    public void setAmount(double newAmount){
+        this.amount = newAmount;
+    }
+
+    public String getDate(){
+        return date;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public String toString(){
+        return "  MONTHLY EXPENSES\n  Date: "+date+"\n  Description: "+description+"\n  Amount: "+amount;
+    }
 
 }
--- Module_5/ExpenseTracker/ValidatorIO.java
+++ Module_5/ExpenseTracker/ValidatorIO.java
@@ -1,5 +1,64 @@
 package Module_5.ExpenseTracker;
 
+import java.util.*;
+
 public class ValidatorIO {
-    
+    public static int getInt(Scanner sc, String prompt){
+        int input = 0;
+
+        boolean isValid = false;
+
+        while (!isValid){
+            System.out.print(prompt);
+
+            if (sc.hasNextInt()){
+                input = sc.nextInt();
+                isValid = true;
+            }
+            else {
+                System.out.println("\n  Error! Invalid integer value.\n");
+            }
+            sc.nextLine();
+        }
+        return input;
+    }
+
+    public static double getDouble(Scanner sc, String prompt){
+        double input = 0;
+
+        boolean isValid = false;
+
+        while(!isValid){
+            System.out.print(prompt);
+
+            if (sc.hasNextDouble()){
+                input = sc.nextDouble();
+                isValid = true;
+            }
+            else{
+                System.out.println("\n  Error! Invalid double value.\n");
+            }
+            sc.nextLine();
+        }
+        return input;
+    }
+    public static String getString(Scanner sc, String prompt){
+        String input = "0";
+
+        boolean isValid = false;
+
+        while(!isValid){
+            System.out.print(prompt);
+
+            if (sc.hasNext()){
+                input = sc.next();
+                isValid = true;
+            }
+            else{
+                System.out.println("\n  Error! Invalid input.\n");
+            }
+            sc.nextLine();
+        }
+        return input;
+    }
 }
