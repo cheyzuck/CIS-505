64d2b32509746d989167348518d66caa274b3022
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-02 10:33:32 -0400

    Build cont

--- Module_5/ExpenseTracker/TestExpenseTracker.java
+++ Module_5/ExpenseTracker/TestExpenseTracker.java
@@ -4,5 +4,18 @@ import java.util.*;
 import java.io.*;
 
 public class TestExpenseTracker {
-    
+
+    public static void displayMenu(){
+        System.out.println("  MENU OPTIONS");
+        System.out.println("    1. View Transactions");
+        System.out.println("    2. Add Transactions");
+        System.out.println("    3. View Expense");
+        System.out.print("  Please choose an option: ");
+    }
+    public static void main(String[] args){
+        System.out.println("  Welcome to the Expense Tracker");
+        displayMenu();
+        Scanner userIn = new Scanner();
+        
+    }
 }
