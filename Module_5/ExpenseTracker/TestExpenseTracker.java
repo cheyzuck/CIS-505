6882fd99840ef7fce779f0e27e73d9c2a9ce7cbe
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-02 10:29:46 -0400

    Build Cont.

--- Module_5/ExpenseTracker/TestExpenseTracker.java
+++ Module_5/ExpenseTracker/TestExpenseTracker.java
@@ -1,5 +1,8 @@
 package Module_5.ExpenseTracker;
 
+import java.util.*;
+import java.io.*;
+
 public class TestExpenseTracker {
     
 }
--- Module_5/ExpenseTracker/TransactionIO.java
+++ Module_5/ExpenseTracker/TransactionIO.java
@@ -1,5 +1,36 @@
 package Module_5.ExpenseTracker;
 
+import java.io.*;
+import java.util.*;
+
 public class TransactionIO {
+    final private static String FILE_NAME = "expenses.txt";
+    private static File file = new File(FILE_NAME);
+
+    public static void bulkInsert(ArrayList<Transaction> transactions) throws IOException{
+        PrintWriter output = null;
+
+        if (file.exists()){
+            output = new PrintWriter(new FileOutputStream(new File(FILE_NAME), true));
+        } 
+        else{
+            output = new PrintWriter(FILE_NAME);
+        }
+
+        for (Transaction tran : transactions){
+            output.print(tran.getDate() + " ");
+            output.print(tran.getDescription() + " ");
+            output.println(tran.getAmount());
+        }
+        output.close();
+    }
+
+    public static ArrayList<Transaction> findAll(){
+        Scanner input = new Scanner (FILE_NAME);
+        while (input.hasNext()){
+            
+        }
+    }
+
     
 }
