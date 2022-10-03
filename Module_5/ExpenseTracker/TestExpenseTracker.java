4ceab5db57f46187bc6a54e965a7b924fddf75a6
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-02 21:42:17 -0400

    Build Cont

--- Module_5/ExpenseTracker/TestExpenseTracker.java
+++ Module_5/ExpenseTracker/TestExpenseTracker.java
@@ -15,6 +15,8 @@ public class TestExpenseTracker {
         return " ";
     }
     public static void main(String[] args) throws IOException{
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
         System.out.println("  Welcome to the Expense Tracker");
         System.out.println("");
         do{
@@ -25,15 +27,12 @@ public class TestExpenseTracker {
                 System.out.println(TransactionIO.findAll());
             } else if (input == 2){
                 String c = "y";
-                ArrayList<Transaction> transactions = new ArrayList<>();
 
                 while (c.equalsIgnoreCase("y")){
                     String description = ValidatorIO.getString(sc, "\n  Enter the description: ");
                     double amount = ValidatorIO.getDouble(sc, "  Enter the amount: ");
-                
-                    Transaction transaction = new Transaction(description, amount);
 
-                    transactions.add(transaction);
+                    transactions.add(new Transaction(description, amount));
 
                     c = ValidatorIO.getString(sc, "\n  Add another transaction? (y/n): ");
                 }try{
@@ -43,7 +42,6 @@ public class TestExpenseTracker {
                 }
             } else if (input == 3){
                 double monthlyExpense = 0;
-                ArrayList<Transaction> transactions = new ArrayList<>();
                 TransactionIO.findAll();
                 for (Transaction transaction : transactions){
                     monthlyExpense += transaction.getAmount();
