cd3f60fddfcd26796c1c031d5570916183482a66
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-02 23:44:59 -0400

    Final w/ Comments

--- Module_5/ExpenseTracker/TestExpenseTracker.java
+++ Module_5/ExpenseTracker/TestExpenseTracker.java
@@ -1,5 +1,8 @@
 /* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University. 
 
+Java Files. W3 Schools. (2022). Retrieved 20 September 2022, from https://www.w3schools.com/java/java_files.asp.
+Modifications by C. Zuck, 2022.
+
 Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
 
 Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved. */
@@ -10,8 +13,9 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-public class TestExpenseTracker {
+public class TestExpenseTracker { /* Begin TestExpenseTracker class. */
 
+    /* This method displays the menu. */
     public static String menu(){
         System.out.println("  MENU OPTIONS");
         System.out.println("    1. View Transactions");
@@ -20,18 +24,21 @@ public class TestExpenseTracker {
         System.out.print("  Please choose an option: ");
         return " ";
     }
+    /* This is the main method that allows the program to run. It throws an IO Exception. */
     public static void main(String[] args) throws IOException{
+        /* This is an attribute defined outside the if/else if/else... statements to simplify code. */
         ArrayList<Transaction> transactions = new ArrayList<>();
 
         System.out.println("  Welcome to the Expense Tracker");
         System.out.println("");
-        do{
+        do{ /* Begin do... while loop to allow user to continue for as long as they like. */
             Scanner sc = new Scanner(System.in);
-            int input = ValidatorIO.getInt(sc, menu());
+            int input = ValidatorIO.getInt(sc, menu()); /* This validates user input. */
+            /* Begin if... else if... statement to attest to the three possibilities in the menu. */
             if (input == 1){
                 TransactionIO.findAll();
-                System.out.println(TransactionIO.findAll());
-            } else if (input == 2){
+                System.out.println(TransactionIO.findAll()); /* This should show all transactions in file. */
+            } else if (input == 2){ /* This allows the user to enter any number of transactions. */
                 String c = "y";
 
                 while (c.equalsIgnoreCase("y")){
@@ -41,21 +48,21 @@ public class TestExpenseTracker {
                     transactions.add(new Transaction(description, amount));
 
                     c = ValidatorIO.getString(sc, "\n  Add another transaction? (y/n): ");
-                }try{
+                }try{ /* This tries the add those transactions to the file. */
                     TransactionIO.bulkInsert(transactions);
-                } catch (IOException e){
+                } catch (IOException e){ /* This catches an exception if its made. */
                     System.out.println("\n  Exception: " + e.getMessage());
                 }
-            } else if (input == 3){
+            } else if (input == 3){ /* This displays monthly expenses. */
                 double monthlyExpense = 0;
                 TransactionIO.findAll();
                 for (Transaction transaction : transactions){
                     monthlyExpense += transaction.getAmount();
                 }
                 System.out.println("  Your total monthly expense is "+ String.format("$%6.2f", monthlyExpense));
-            } else if (input != 1|| input !=2||input !=3){
+            } else if (input != 1|| input !=2||input !=3){ /* This throws an error if the user does not input 1, 2, or 3. */
                 System.out.print("  Error! Invalid option.");
-            }
+            } /* This allows the user to choose to continue if they wish. */
             System.out.println("");
             System.out.print("  Continue? (y/n): ");
             Scanner userContinue = new Scanner(System.in);
@@ -69,6 +76,6 @@ public class TestExpenseTracker {
             } else if (continueChoice.equalsIgnoreCase("y")){
                 continue;
             }
-        } while (true);
+        } while (true); /* This continues the loop if the conditions are met. */
     }
-}
+}/* End TestExpenseTrackerApp. */
--- Module_5/ExpenseTracker/ValidatorIO.java
+++ Module_5/ExpenseTracker/ValidatorIO.java
@@ -1,5 +1,8 @@
 /* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University. 
 
+Java Files. W3 Schools. (2022). Retrieved 20 September 2022, from https://www.w3schools.com/java/java_files.asp.
+Modifications by C. Zuck, 2022.
+
 Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
 
 Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved. */
@@ -8,15 +11,17 @@ package Module_5.ExpenseTracker;
 
 import java.util.*;
 
-public class ValidatorIO {
+public class ValidatorIO { /* Begin ValidatorIO Class. */
+    /* This method retrieves the integer for the menu. */
     public static int getInt(Scanner sc, String prompt){
+        /* These are the attributes. */
         int input = 0;
-
         boolean isValid = false;
 
+        /* This while loop allows the input to be validated and allow the scanner to move forward. */
         while (!isValid){
             System.out.print(prompt);
-
+            /* This if/else statement shows what to do if the input is not valid. */
             if (sc.hasNextInt()){
                 input = sc.nextInt();
                 isValid = true;
@@ -29,6 +34,7 @@ public class ValidatorIO {
         return input;
     }
 
+    /* This method retrieves the double amount and validates that is it possible. */
     public static double getDouble(Scanner sc, String prompt){
         double input = 0;
 
@@ -36,7 +42,7 @@ public class ValidatorIO {
 
         while(!isValid){
             System.out.print(prompt);
-
+            /* This if/else statement shows what to do if the input is not valid. */
             if (sc.hasNextDouble()){
                 input = sc.nextDouble();
                 isValid = true;
@@ -48,7 +54,7 @@ public class ValidatorIO {
         }
         return input;
     }
-    
+    /* This method retrieves the string and validates that is it possible. */
     public static String getString(Scanner sc, String prompt){
         String input = "0";
 
@@ -68,4 +74,4 @@ public class ValidatorIO {
         }
         return input;
     }
-}
+}/* End ValidatorIO class. */
