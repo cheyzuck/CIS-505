5c43b62b58b1058556d74311ccbf8ecb77810e08
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-08 16:12:39 -0400

    Initial Build

--- Module_6/ComposerApp/TestComposerApp.java
+++ Module_6/ComposerApp/TestComposerApp.java
@@ -1,5 +1,19 @@
 package Module_6.ComposerApp;
 
 public class TestComposerApp {
-    
+    public static void displayMenu(){
+        System.out.println("  MENU OPTIONS");
+        System.out.println("    1. View Composers");
+        System.out.println("    2. Find Composer");
+        System.out.println("    3. Add Composer");
+        System.out.println("    4. Exit");
+        System.out.println("");
+        System.out.print("  Please choose an option: ");
+    }
+
+    public static void main(String[] args){
+        System.out.println("  Welcome to the Composer App");
+        displayMenu();
+        
+    }
 }
