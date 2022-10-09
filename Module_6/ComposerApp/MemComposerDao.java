e3611d228b35c78de856f1d29b7aaf944ece93c1
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-09 14:40:26 -0400

    Build Cont

--- Module_6/ComposerApp/MemComposerDao.java
+++ Module_6/ComposerApp/MemComposerDao.java
@@ -3,9 +3,19 @@ package Module_6.ComposerApp;
 import java.util.*;
 
 public class MemComposerDao {
-    private List<Composer> composers;
+    private List<Composer> composers = new ArrayList<>();
 
     public MemComposerDao(){
+        Composer composer1 = new Composer(1007,"Ludwig Van Beethoven","Classical");
+        Composer composer2 = new Composer(1008, "Johann Sebastian Bach", "Classical");
+        Composer composer3 = new Composer(1009, "Wolfgang Amadeus Mozart", "Classical");
+        Composer composer4 = new Composer (1010, "Johannes Brahms", "Classical");
+        Composer composer5 = new Composer(1011, "Joseph Haydn", "Classical");
+        composers.add(composer1);
+        composers.add(composer2);
+        composers.add(composer3);
+        composers.add(composer4);
+        composers.add(composer5);
     }
 
     public List<Composer> findAll(){
@@ -13,7 +23,11 @@ public class MemComposerDao {
         return composers;
     }
     public Composer findBy(int id){
-        Composer composer = new Composer();
+        System.out.println("  --DISPLAYING COMPOSERS--");
+        Composer composer = new Composer();;
+        
+
+        }
         return composer;
     }
 
