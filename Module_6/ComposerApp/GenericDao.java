148a65772c8a4af7ffc1e684145e1925e706c580
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-10 00:18:03 -0400

    Final with Comments

--- Module_6/ComposerApp/GenericDao.java
+++ Module_6/ComposerApp/GenericDao.java
@@ -8,8 +8,8 @@ package Module_6.ComposerApp;
 
 import java.util.*;
 
-public interface GenericDao<E, K> {
-    List <E> findAll();
-    E findBy(K key);
-    void insert(E entity);
-}
+public interface GenericDao<E, K> { /* This is a GenericDao that establishes abstract methods. */
+    List <E> findAll(); /* This is an abstract method to findAll. */
+    E findBy(K key); /* This is an abstract method to findBy. */
+    void insert(E entity); /* This is an abstract method to insert an entity. */
+} /* End GenericDao class. */
