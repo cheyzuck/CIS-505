0df47dc9bc812c43dc91de21f7399c53e874f27c
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-10 00:12:36 -0400

    Final with Comments

--- Module_6/ComposerApp/GenericDao.java
+++ Module_6/ComposerApp/GenericDao.java
@@ -1,3 +1,9 @@
+/* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University. 
+
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+
+Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved. */
+
 package Module_6.ComposerApp;
 
 import java.util.*;
