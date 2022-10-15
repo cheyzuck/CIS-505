859ac3fb277fb23c2d9052674b6064425696788a
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-15 14:08:51 -0400

    i'm not sure i even set this up right

---
+++ FutureValueApp/ZuckFutureValueApp.java
@@ -0,0 +1,14 @@
+package FutureValueApp;
+
+import java.util.*;
+import javafx.application.Application;
+
+public class ZuckFutureValueApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+
+    
+}
