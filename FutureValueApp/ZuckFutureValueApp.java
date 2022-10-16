d7d8c39e12a161db9cf655e29dcb6e7fedf28057
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-16 10:08:55 -0400

    Build Cont

--- FutureValueApp/ZuckFutureValueApp.java
+++ FutureValueApp/ZuckFutureValueApp.java
@@ -4,6 +4,17 @@ import java.util.*;
 import javafx.application.Application;
 
 public class ZuckFutureValueApp extends Application {
+    private TextField text1;
+    private TextField text2;
+    private TextArea area1;
+    private Label lblMonthlyPayment = new Label("Monthly Payment: ");
+    private Label lblInterestRate = new Label("Interest Rate: ");
+    private Label label3;
+    private Label label4;
+    private Label label5;
+    private ComboBox int combo;
+    private Button btnCalculate = new Button("Calculate");
+    private Button button2;
 
     @Override
     public void start(Stage primaryStage){
