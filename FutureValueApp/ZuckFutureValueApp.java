0574702b44c791e509323a6d3c9b2ab11d31d573
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-16 10:26:47 -0400

    Initial Build

--- FutureValueApp/ZuckFutureValueApp.java
+++ FutureValueApp/ZuckFutureValueApp.java
@@ -9,17 +9,24 @@ public class ZuckFutureValueApp extends Application {
     private TextArea area1;
     private Label lblMonthlyPayment = new Label("Monthly Payment: ");
     private Label lblInterestRate = new Label("Interest Rate: ");
-    private Label label3;
-    private Label label4;
-    private Label label5;
+    private Label lblYears = new Label("Years: ");
+    private Label lblClear = new Label("Clear");
+    private Label lblCalculate = new Label("Calculate");
     private ComboBox int combo;
     private Button btnCalculate = new Button("Calculate");
-    private Button button2;
+    private Button btnClear = new Button("Clear");
 
     @Override
     public void start(Stage primaryStage){
-
+        GridPane pane = new GridPane(Pos.CENTER, 11.5, 12.5, 13.5, 14.5, 5.5, 5.5)
+        lblInterestRateFormat.setTextFill(Color.RED);
+        pane.add(lblInterestRateFormat, 1, 2);
+        GridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT);
+        HBox actionBtnContainer = new HBox();
+        actionBtnContainer.setPadding(new Insets(15,0,15,30));
+        actionBtnContainer.setSpacing(10);
+        actionBtnContainer.getChildren().add(btnClear);
+        actionBtnContainer.getChildren().add(btnCalculate);
+        pane.add(actionBtnContainer, 1, 4);
     }
-
-    
 }
