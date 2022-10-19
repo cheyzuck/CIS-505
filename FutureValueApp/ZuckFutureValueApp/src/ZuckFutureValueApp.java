12f37a18d7dc049c37c535ca42b8555c3d852706
Cheyenne <105511806+cheyzuck@users.noreply.github.com>
2022-10-19 12:35:04 -0400

    Comments which didn't save.

--- FutureValueApp/ZuckFutureValueApp/src/ZuckFutureValueApp.java
+++ FutureValueApp/ZuckFutureValueApp/src/ZuckFutureValueApp.java
@@ -1,3 +1,15 @@
+/* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University.
+
+Cool IT Help. (2020). How to setup JavaFx Environment in Visual Studio code? [Video]. Youtube. https://youtu.be/H67COH9F718.
+
+JavaFX Tutorial - JavaFX Introduction. Java2s.com. (2022). Retrieved 15 October 2022, from http://www.java2s.com/Tutorials/Java/JavaFX/index.htm.
+
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+
+Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved. */
+
+
+/* Imports */
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -13,8 +25,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class ZuckFutureValueApp extends Application {
-
+public class ZuckFutureValueApp extends Application { /* Begin ZuckFutureValue App, extending JavaFX Application. */
+    /* These are the attributes of the application. */
     private TextField txtMonthlyPayment = new TextField();
     private TextField txtInterestRate = new TextField();
     private TextArea area1 = new TextArea();
@@ -27,10 +39,12 @@ public class ZuckFutureValueApp extends Application {
     private Button btnCalculate = new Button("Calculate");
     private Button btnClear = new Button("Clear");
 
+    /* This overrides the start method. */
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Zuck Future Value App");
+        primaryStage.setTitle("Zuck Future Value App"); /* Title set. */
 
+        /* This creates the pane and adds all my attributes to it. */
         GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
@@ -43,11 +57,13 @@ public class ZuckFutureValueApp extends Application {
         pane.add(lblInterestRate, 0, 1);
         pane.add(lblYears, 0, 3);
         pane.add(years, 1, 3);
-
+        
+        /* This sets the InterestRateFormat to Red, assigns it to the pane, and positions it. */
         lblInterestRateFormat.setTextFill(Color.RED);
         pane.add(lblInterestRateFormat, 1, 2);
         GridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT);
 
+        /* This creates a container for the buttons, adds them to the pane, and controls the buttons. */
         HBox actionBtnContainer = new HBox();
         actionBtnContainer.setPadding(new Insets(15, 0, 15, 30));
         actionBtnContainer.setSpacing(10);
@@ -55,12 +71,14 @@ public class ZuckFutureValueApp extends Application {
         actionBtnContainer.getChildren().add(btnCalculate);
         pane.add(actionBtnContainer, 1, 4);
 
+        /* This creates the scene, adds the pane to the scene, the scene to the stage, and then shows the stage. */
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+    } /* End override of the Start method. */
 
+    /* This is the main method that runs the application. */
     public static void main(String[] args) {
-        launch(args);
-    }
-}
+        launch(args); /* This launches the arguments in the main method. */
+    } /* End main method. */
+} /* End ZuckFutureValueApp. */
