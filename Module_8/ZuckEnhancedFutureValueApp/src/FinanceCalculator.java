ffde4b083de4e37b9294d88e42364ee5cd553770
Cheyenne <chzuck@my365.bellevue.edu>
2022-10-23 23:27:37 -0400

    Final with Comments Merged

---
+++ Module_8/ZuckEnhancedFutureValueApp/src/FinanceCalculator.java
@@ -0,0 +1,17 @@
+package Module_8.ZuckEnhancedFutureValueApp.src;
+
+/* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University.*/
+
+public class FinanceCalculator { /* Begin FinanceCalculator Class. */
+    /* This sets the variable to 12 as a constant. */
+    private static int MONTHS_IN_YEAR = 12;
+
+    /* This method calculates the futureValue based on the arguments it takes. */
+    public static double calculateFutureValue(double monthlyPayment, double rate, int years){
+        int months = years * MONTHS_IN_YEAR;
+        double presentValue = monthlyPayment * months;
+        double interestRate = (1+rate/100);
+        double futureValue = presentValue * (Math.pow(interestRate,months));
+        return futureValue;
+    } /* End calculateFutureValue method. */
+} /* End FinanceCalculator class. */
--- Module_8/ZuckEnhancedFutureValueApp/src/ZuckEnhancedFutureValueApp.java
+++ Module_8/ZuckEnhancedFutureValueApp/src/ZuckEnhancedFutureValueApp.java
@@ -1,4 +1,4 @@
-package Module_8;
+package Module_8.ZuckEnhancedFutureValueApp.src;
 
 /* Zuck, C. (2022). CIS 505 Intermediate Java Programming. Bellevue University.
 
@@ -6,12 +6,16 @@ Cool IT Help. (2020). How to setup JavaFx Environment in Visual Studio code? [Vi
 
 JavaFX Tutorial - JavaFX Introduction. Java2s.com. (2022). Retrieved 15 October 2022, from http://www.java2s.com/Tutorials/Java/JavaFX/index.htm.
 
+Set and Clear Value for Text Field. Java2s.com. (2022). Retrieved 23 October 2022, from http://www.java2s.com/Code/Java/JavaFX/SetandclearvalueforTextField.htm.
+
 Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
 
 Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved. */
 
 /* Imports */
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -26,6 +30,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,15 +38,19 @@ public class ZuckEnhancedFutureValueApp extends Application { /* Begin ZuckFutur
     /* These are the attributes of the application. */
     private TextField txtMonthlyPayment = new TextField();
     private TextField txtInterestRate = new TextField();
-    private TextArea txtResults = new TextArea("The Future value is: ");
+    private TextArea txtResults = new TextArea();
     private Label lblMonthlyPayment = new Label("Monthly Payment: ");
     private Label lblInterestRate = new Label("Interest Rate: ");
     private Label lblYears = new Label("Years: ");
     private Label lblInterestRateFormat = new Label("Enter 11.1% as 11.1");
-    private Label lblFutureValueDate = new Label("Calculation as of " +getDate());
-    private ComboBox<Integer> cbYears = new ComboBox<Integer>();
+    private Label lblFutureValueDate = new Label();
+    private Integer[] interestYears = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    private ComboBox<Integer> cbYears = new ComboBox<>();
     private Button btnCalculate = new Button("Calculate");
     private Button btnClear = new Button("Clear");
+
+    /* This formats numbers into $0.00 format. */
+    private DecimalFormat df = new DecimalFormat("$0.00");
 
     /* This overrides the start method. */
     @Override
@@ -56,11 +65,16 @@ public class ZuckEnhancedFutureValueApp extends Application { /* Begin ZuckFutur
         pane.setVgap(5.5);
         pane.add(txtMonthlyPayment, 1, 0);
         pane.add(txtInterestRate, 1, 1);
-        pane.add(txtResults, 0, 5);
+        pane.add(txtResults, 0, 6, 2, 1);
         pane.add(lblMonthlyPayment, 0, 0);
         pane.add(lblInterestRate, 0, 1);
         pane.add(lblYears, 0, 3);
         pane.add(cbYears, 1, 3);
+        pane.add(lblFutureValueDate,0 ,5);
+
+        /* This takes my list and adds it to my combobox. */
+        ObservableList<Integer> years = FXCollections.observableArrayList(interestYears);
+        cbYears.getItems().addAll(years);
         
         /* This sets the InterestRateFormat to Red, assigns it to the pane, and positions it. */
         lblInterestRateFormat.setTextFill(Color.RED);
@@ -75,19 +89,26 @@ public class ZuckEnhancedFutureValueApp extends Application { /* Begin ZuckFutur
         actionBtnContainer.getChildren().add(btnCalculate);
         pane.add(actionBtnContainer, 1, 4);
 
+        /* This sets the buttons to take actions based on the methods they call. */
+        btnClear.setOnAction(e -> clearFormFields());
+        btnCalculate.setOnAction(e -> calculateResults());
+
         /* This creates the scene, adds the pane to the scene, the scene to the stage, and then shows the stage. */
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.show();
     } /* End override of the Start method. */
 
+    /* This method clears the form and sets the text/values to null or empty. */
     private void clearFormFields(){
-        txtMonthlyPayment = toString("");
-        txtInterestRate = toString("");
-        txtResults = toString("");
-        lblFutureValueDate = toString("");
-        cbYears = 0;
+        txtMonthlyPayment.setText("");
+        txtInterestRate.setText("");
+        txtResults.setText("");
+        lblFutureValueDate.setText("");
+        cbYears.setValue(null);
     }
+
+    /* This method gets the date and sets it. */
     private String getDate(){
         SimpleDateFormat simpleDate = new SimpleDateFormat("MM/dd/yyyy");
         Date today = new Date();
@@ -95,8 +116,13 @@ public class ZuckEnhancedFutureValueApp extends Application { /* Begin ZuckFutur
         return outputDate;
     }
 
-    private double calculateResults(){
-        FinanceCalculator.calculateFutureValue(txtMonthlyPayment, txtInterestRate, cbYears);
+    /* This method calculates the results based on the Finance Calculator, sets the values of the lblFutureValueDate and txtResults fields. */
+    private void calculateResults(){
+        Double monthlyPayment = Double.valueOf(txtMonthlyPayment.getText());
+        Double interestRate = Double.valueOf(txtInterestRate.getText());
+        int years = cbYears.getValue();
+        lblFutureValueDate.setText("Calculation as of "+getDate());
+        txtResults.setText("The future value is " + df.format(FinanceCalculator.calculateFutureValue(monthlyPayment, interestRate, years)));
     }
 
     /* This is the main method that runs the application. */
